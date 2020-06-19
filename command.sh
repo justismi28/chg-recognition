d1922a03be2727ff5d1ad2f129e50a3b252960c1
Curtis Porter <curtis.porter@chghealthcare.com>
2020-06-19 07:59:56 -0600

    fix: set environment variables in docker run

--- command.sh
+++ command.sh
@@ -3,4 +3,4 @@ export OKTA_ISSUER_URL="https://chghealthcare.oktapreview.com/oauth2/auskmtjacfE
 export OKTA_CLIENT_ID="0oas5s11wsNsbTO0M0h7"
 export MONGO_URL="mongodb+srv://admin:UJ9VR5s3BaBz2zE5@cluster0-g3lzu.mongodb.net/test?retryWrites=true&w=majority"
 echo "Running with MONGO_URL: $MONGO_URL"
-docker run --name chg-recognition --env MONGO_URL -p8777:8081 -d --restart always chg-recognition:$VERSION
+docker run --name chg-recognition --env MONGO_URL --env OKTA_ISSUER_URL --env OKTA_CLIENT_ID -p8777:8081 -d --restart always chg-recognition:$VERSION
