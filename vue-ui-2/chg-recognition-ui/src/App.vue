ca65378a33d8911db9cd46d67a79aa6b794ce904
Curtis Porter <curtis.porter@chghealthcare.com>
2020-06-19 16:34:15 -0600

    feat: use tabs for navigation

--- vue-ui-2/chg-recognition-ui/src/App.vue
+++ vue-ui-2/chg-recognition-ui/src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hr/>
+    <b-nav tabs>
+      <b-nav-item href="#/">Nomination Form</b-nav-item>
+      <b-nav-item href="#/history">History</b-nav-item>
+      <b-nav-item href="#/users">Users</b-nav-item>
+    </b-nav>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { NavPlugin } from 'bootstrap-vue'
+
+Vue.use(NavPlugin)
+
 export default {
   name: 'App'
 }
